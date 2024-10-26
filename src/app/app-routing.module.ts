8b11ca605b0fd8766a2a02017b0ce9accd17820e
USER <rouakharrat662002@gmail.com>
2024-10-26 17:43:53 +0100

    page secondaire du choix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { BodyComponent } from './body/body.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: BodyComponent },  // Route par défaut pour le BodyComponent
+  { path: 'welcome', component: WelcomePageComponent },  // Route pour la page de bienvenue
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,24 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import { BrowserModule} from '@angular/platform-browser';
+import { MatDividerModule } from '@angular/material/divider';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BodyComponent } from './body/body.component';
-
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BodyComponent
+    BodyComponent,
+    WelcomePageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatDividerModule
   ], 
-  providers: [
-    provideClientHydration()
-  ],
+ 
   bootstrap: [AppComponent]
 })
 export class AppModule { }
